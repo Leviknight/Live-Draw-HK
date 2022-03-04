8e6d3c3eefb0624d8df7ae490a7f6710bfbcd356
Leviknight <58016222+Leviknight@users.noreply.github.com>
2022-03-04 15:09:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,13 +53,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 <script>
  $(document).ready(function() {
- $(".hk").load("hk.php");
+ $(".hk").load("hk.html");
  $.ajaxSetup({ cache: false });
 });
 </script>
 <script>
  $(document).ready(function() {
-   $(".result").load("result.php");
+   $(".result").load("result.html");
    $.ajaxSetup({ cache: false });
 });
 </script>
@@ -74,43 +74,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
  
  
     
-<!--<div class="banner-floattop">-->
 
-<!--<a href="https://91.233.198.25/" rel="nofollow noopener" target="_blank"><img alt="GITARTOGEL" class="aligncenter" height="90" src="https://1.bp.blogspot.com/-CE6VNmtrdCo/YSxw4NL1jmI/AAAAAAAAADA/fVlJgqFEP4QlrqUFf0jeWDMagMtkFbcPgCLcBGAsYHQ/s728/GITARTOGEL_0370_728x90.gif" title="GITARTOGEL" width="728" /></a>-->
-
-<!--<a href="https://91.233.198.9/" rel="nofollow noopener" target="_blank"><img alt="RGOBET" class="aligncenter" height="90" src="https://1.bp.blogspot.com/-_Fww5JAA9KI/YSxw4IjA8tI/AAAAAAAAADE/_2g_qsaV_ZkpICSk2m8Zx3sQKjxpTRFAgCLcBGAsYHQ/s728/RGOBET_0369_728x90.gif" title="RGOBET" width="728" /></a>-->
-<!--</div>-->
-<!--<div class="banner-floatbot">-->
-
-<!--<a href="https://91.233.198.20/" rel="nofollow noopener" target="_blank"><img alt="SEPAKBOLACC" class="aligncenter" height="90" src="https://1.bp.blogspot.com/-UOsMLHkb68Y/YSxw5TrxTlI/AAAAAAAAADM/Jy827cho0-YNg_lfgkCEGyzYCTg6MXplwCLcBGAsYHQ/s728/SBCC_0387_728-X-90.gif" title="SEPAKBOLACC" width="728" /></a>-->
-
-<!--<a href="https://91.233.198.5/" rel="nofollow noopener" target="_blank"><img alt="WIGOBET" class="aligncenter" height="90" src="https://1.bp.blogspot.com/-eJkjs_yHrFM/YSxw6N-D9II/AAAAAAAAADQ/wKU2l1rEfMEFYA2T_WzsgLk33aKrGvJpQCLcBGAsYHQ/s16000/WIGOBET_0389_728x90.gif" title="WIGOBET" width="728" /></a>-->
-<!--</div>-->
-
-<div class="container">
-<div class="head">
-
-<a class="logo" href="img/logo-live-hk.png"></a>
-
-</div>
-<div class="banner1">
-    
-    <h1><a class="title-head" href="index.html" title="Live Draw HK">giyanahana@gmail.com</a></h1>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-
-
-
-</div>
-<div class="menu">
-<a href="http://nagaprediksi.xyz/" title="LIVE DRAW SDY" target="_blank">LIVE DRAW SDY</a>
-<a href="http://4angka.xyz/" title="LIVE DRAW SGP" target="_blank">LIVE DRAW SGP</a>
-<a href="http://havadea.com/" title="PAITO WARNA" target="_blank">PAITO WARNA</a>
-<a href="https://havadea.com/link-alternatif-togel-terpercaya/" title="LINK ALTERNATIF TOGEL" target="_blank">LINK ALTERNATIF TOGEL</a>
-</div>
 <div class="content">
 
 <h1><a class="title-head" href="index.html" title="Live Draw HK">Live Draw HK</a></h1>
@@ -125,321 +89,13 @@ berkisar sekitar pukul 23.00 WIB malam.</p>
 
 <p>Live draw togel HK / Hongkong akan di mulai setiap hari pada pukul 22.30 WIB hingga pukul 23.00 WIB malam.</p>
 
-<div class="hk">
-<table>
-<tr>
-<td class="tanggal" colspan="2"><?php 
-$html = file_get_contents("http://178.128.89.216/");
-$xpath_doc = new DOMDocument();
+<div class="hk"></div>
 
-libxml_use_internal_errors(TRUE);
-
-if(!empty($html)){
-
-	$xpath_doc -> loadHTML($html);
-	libxml_clear_errors();
-	$xpath = new DOMXPath($xpath_doc);
-
-	$caritglhk = $xpath -> query('//*[@id="post-15"]/div[2]/table[1]/tbody/tr[1]/td');
-
-
-	if ($caritglhk -> length > 0){
-		foreach($caritglhk as $tglhk){
-
-                echo $tglhk -> nodeValue;
-}
-	}
-	
-}
-
-?></td>
-</tr>
-<tr>
-<td class="iklan" colspan="2">
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-</td> 
-</tr>
-<tr>
-<td class="prize" colspan="1">PRIZE 1</td>
-<td class="prizenum" colspan="1" style="color: #a10e1c;"></td>
-</tr>
-
-
-
-<tr>
-<td class="iklan" colspan="2">
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-</td> 
-</tr>
-
-<tr>
-<!-- <td class="iklan" colspan="2">
-<a href="https://bit.ly/2TlxAGE" target="_blank"><img src="../1.bp.blogspot.com/-zPCf7OLTFzw/YN66ra2tG-I/AAAAAAAAFBw/Ud8JFg6MaNIAvOkTqZTv3_T6dM5CxhU6wCNcBGAsYHQ/s728/vegas6D-680x100.gif" /></a>
-
-</td> -->
-</tr>
-
-
-
-<tr>
-<td class="prize" colspan="1">PRIZE 2</td>
-<td class="prizenum" colspan="1"></td>
-</tr>
-
-<tr>
-<td class="prize" colspan="1">PRIZE 3</td>
-<td class="prizenum" colspan="1"></td>
-</tr>
-<tr>
-<td class="iklan" colspan="2">
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-
-</td> 
-</tr>
-<tr>
-<td class="tanggal" colspan="2">STARTER</td>
-</tr>
-
-<tr>
-<td class="othernum" colspan="1"></td>
-</td>
-<td class="othernum" colspan="1"></td>
-</td>
-</tr>
-
-<tr>
-<td class="othernum" colspan="1"></td>
-</td>
-<td class="othernum" colspan="1"></td>
-</td>
-</tr>
-
-<tr>
-<td class="iklan" colspan="2">
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-</td>
-
-<tr>
-<td class="tanggal" colspan="2">CONSOLATION</td>
-</tr>
-
-<tr>
-<td class="othernum" colspan="1"></td>
-</td>
-<td class="othernum" colspan="1"></td>
-</td>
-</tr>
-
-<tr>
-<td class="othernum" colspan="1"></td>
-</td>
-<td class="othernum" colspan="1"></td>
-</td>
-</tr>
-
-<tr>
-<td class="othernum" colspan="1"></td>
-</td>
-<td class="othernum" colspan="1"></td>
-</td>
-</tr>
-
-<tr>
-<td class="othernum" colspan="1"></td>
-</td>
-<td class="othernum" colspan="1"></td>
-</td>
-</tr>
-<tr>
-<td class="iklan" colspan="2">
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-
-
-
-</table>
-</div>
-
-
-<div class="banner2">
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-
-</div>
 <h2 style="margin-top: 20px;"><a class="title-head" href="index.html" title="Hasil Result HK">Hasil Result HK</a></h2>
 <p>Berikut ini <b>Live HK</b> juga menampilkan hasil result HK untuk sebulan terakhir sebagai informasi result terakhir dari togel Hongkong.</p>
 <p>- Memanggil Data Keluaran Terbaru Hari Ini,Harap tunggu beberapa saat.<p>
 
 <div class="result">
-    <table>
-<tr>
-<td class="t1 kepala">TANGGAL</td>
-<td class="t2 kepala">HARI</td>
-<td class="t3 kepala">PRIZE 1</td>
-</tr>
-
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-<tr>
-<td class="t1"></td>
-<td class="t2"></td>
-<td class="t3"></td>
-</tr>
-</table>
 </div>
 
 <p>Sistem Pemutaran Nomor Live Draw HK Tercepat merupakan salah satu pengundian yang menggunakan sistem yang di lakukan oleh pihak Hongkong Pools secara langsung dalam pengeluaran HKG pemutaran itu dibuat khusus, agar para pencinta Hongkong Pools dapat dengan mudah memahami hasil akhir dari pemutaran Live Draw Hongkong, tujuan kami membuat website ini agar para pemain semua dapat dengan leluasa membuka dari Indonesia di karenakan situs resminya yaitu hongkongpools.com sudah tidak bisa di akses dari Indonesia di karenakan terkena pemblokiran yang di lakukan oleh pemerintah karena mengandung insur judi dan sudah kita ketahui di Indonesia sejak tahun 2004 segala jenis perjudian di ilegalkan. Tapi karena saya sudah membuat website ini kalian akan lebih mudah jika ingin melihat Live Draw HK secara langsung.
@@ -456,31 +112,7 @@ if(!empty($html)){
 <p>Itulah live draw Hk serta result terakhir data pengeluaran togel HK / Hongkong terlengkap, terpercaya serta tercepat yang dicari-cari oleh para pemain togel Hongkong yang ada di Indonesia.
 Selain Togel Hongkong, kami juga menyediakan live draw pasaran togel lain seperti Sydney, Singapore dan sebagainya. Anda dapat memilih live draw togel lainnya melalui link yang kami sediakan di atas situs ini.</p>
 <p>Anda juga bisa memilih agen / bandar / BO togel Online Terpercaya pilihan kami dengan cara memilih salah satu banner / iklan yang tersedia di situs <b>Live HK</b> ini. Semoga beruntung dan salam JP Paus!!!</p>
-<div class="banner2">
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
 
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-<a href="#" target="_blank"><img src="https://1.bp.blogspot.com/-hSIcI8dx5EM/YRQ0XqnyzII/AAAAAAAAACU/KENM98AHQiEqgfemqkYvLgxkxO9K2juZQCLcBGAsYHQ/s728/pasangiklan.gif" /></a>
-
-</div>
- <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4544372,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4544372&101" alt="stats count" border="0"></a></noscript>
-<!-- Histats.com  END  -->
-<h3><a class="title-footer" href="index.html" title="Live Draw HK">Live Draw HK</a> | <a class="title-footer" href="index.html" title="Live Draw Hongkong">Live Draw Hongkong</a> | <a class="title-footer" href="index.html" title="Result HK">Result HK</a></h3>
-
-</div>
 <div class="space">
 
 </div>
